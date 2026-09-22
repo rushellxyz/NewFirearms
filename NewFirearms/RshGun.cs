0d3de9d717ccd6bccb388ad4ca732ba55a52db9c
rushellxyz <asafonnikov@gmail.com>
2026-09-22 09:29:22 +0300

    Revert "ААААААААААААААААААААААААА"
    This reverts commit 5907a38bac4e5dc930db685100be6ad6fc275823.

--- NewFirearms/RshGun.cs
+++ NewFirearms/RshGun.cs
@@ -127,9 +127,6 @@ namespace NewFirearms
 
         public void FixedUpdate()
         {   // TODO Move that into Update?
-            if (Plugin.useCuCore)
-                UpdateSprite(); // ААААААААААААААААААААААААА
-
             if (!Plugin.krokMpEnabled)
                 return;
             if (null == transform.parent || !transform.parent.TryGetComponent<InventorySlot>(out InventorySlot slot) || slot.slot != slot.body.handSlot)
